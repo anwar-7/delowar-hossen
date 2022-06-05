e374956212c4b112850bcfe2ffd09ba3fcd9521d
anwarul1 <anwarjewel007@gmail.com>
2022-06-06 02:49:23 +0600

    update project section

--- src/Constants.js
+++ src/Constants.js
@@ -1,98 +1,44 @@
-import AUTOMATION from './Assets/Projects/3automation.jpg';
-import COAST from './Assets/Projects/coast.jpg';
-import DROPSHIP from './Assets/Projects/dropship.jpg';
-import GOOD_FOOD_DISCOUNTS from './Assets/Projects/goodFoodDiscounts.png';
-import GUAMEDIA from './Assets/Projects/guamedia.jpg';
-import HSP from './Assets/Projects/hsp.png';
-import COSMOS from './Assets/Projects/cosmos.jpg';
-import TECHSPEC from './Assets/Projects/techspec.png';
-import TRAVELWELL from './Assets/Projects/travelwell.png';
+import COAST from './Assets/Projects/project3.png';
+import COSMOS from './Assets/Projects/project1.png';
+import TRAVELWELL from './Assets/Projects/project2.png';
 
 export const PROJECTS = [
   {
     image: COSMOS,
-    name: 'Micorsoft Azure Cosmos DB',
-    technologyUsed: 'ReactJs, Javascript, Redux, knockout.js, Jest',
+    name: 'Tec Mania',
+    technologyUsed:
+      'HTML, CSS, ReactJs, NodeJs, ExpressJs, Mongodb, Firebase, Axios',
     description:
-      "Azure Cosmos DB is Microsoft's proprietary globally distributed, multi-model database service 'for managing data at planet-scale' Azure Cosmos DB is a fully managed NoSQL database for modern app development. Single-digit millisecond response times, and automatic and instant scalability, guarantee speed at any scale.",
-    url: 'https://cosmos.azure.com/',
+      'Warehouse Management website including add or remove items. User can View own added items. Login And registration are secured',
+    url: 'https://inventory-management-p11.web.app/',
   },
   {
     image: TRAVELWELL,
-    name: 'travlwell.com',
-    technologyUsed: 'Shopify, React, typescript, polaris, GraphQL',
+    name: 'Money Master',
+    technologyUsed: 'HTML, CSS, JavaScript ES6',
     description:
-      "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
-    url: 'https://travlwell.com/',
+      'Money Master is a app where user can calculate expanse. User can Calculate saving. User can calculate the remaining',
+    url: 'https://herohossen.github.io/money-master-a5/',
   },
   {
     image: COAST,
-    name: 'Coast',
-    technologyUsed: 'ReactNative, Javascript, Redux',
+    name: 'Car World',
+    technologyUsed: 'HTML, CSS, ReactJs',
     description:
-      'Team collaboration mobile app and online services like slack Message people and groups without sharing phone numbers, communicate with managers and staff all in one place. and discuss or assign tasks to get stuff done',
-    url: 'https://play.google.com/store/apps/details?id=com.fomo.android.app',
-  },
-  {
-    image: AUTOMATION,
-    name: '3automation.com (RPA)',
-    technologyUsed: 'ReactJs, Typescript, Redux',
-    description:
-      'The next-gen Robotic Process Automation (#RPA) platform for retail and telecom domains. The ultimate automation platform for your Shopify, Email, E- commerce shop',
-    url: 'https://www.3automation.com/',
-  },
-  {
-    image: GUAMEDIA,
-    name: 'GuaMedia',
-    technologyUsed: 'ReactNative, Javascript, Redux',
-    description:
-      'Social media mobile app similar to Twitter for china. Social networking GUOMEDIA is an all-in-one social networking platform where users can build their own user profiles, create their own posts, Livestream broadcast and follow each other',
-    url: 'https://play.google.com/store/apps/details?id=com.tiu.guo.media',
-  },
-  {
-    image: TECHSPEC,
-    name: 'TechSpec',
-    technologyUsed: 'ReactJs, Typescript, Redux',
-    description:
-      'TechSpec is e-commerce web app. User can compare his product and purchange. This had a dashboard web app which had the data of orders placed and type of user.',
-    url: '',
-  },
-  {
-    image: HSP,
-    name: 'HSP (Health Solution Plus)',
-    technologyUsed: 'ReactJs, Javascript, Redux',
-    description:
-      'This is a Healthcare sector project which would be developed as replica of a desktop application. My responsibilities in this project are to develop UI, perform searching of medical codes and categories, addition/updation of codes and categories.',
-    url: '',
-  },
-  {
-    image: DROPSHIP,
-    name: 'DropShip Shopify App',
-    technologyUsed:
-      'Shopify, React, typescript, polaris, GraphQL, Nodejs, Koajs',
-    description:
-      'It is customized clothes shopify app which fetch order from store and give option to customize product and send detail to store owner. Also maintain tracking etc of product.',
-    url: '',
-  },
-  {
-    image: GOOD_FOOD_DISCOUNTS,
-    name: 'GoodFoodDiscounts',
-    technologyUsed: 'ReactNative, Javascript, Redux',
-    description:
-      'Good food discouts app is similar like swiggy. Food and hotel can be searched and can be order. Even has facility to add coupan. You can add food in favourite list.',
-    url: '',
+      'Car World is a app where user can find his favorite car. User can choose any 3 car for choice. User can also make the system to choose a random car for him/her.',
+    url: 'https://car-world.netlify.app/',
   },
 ];
 
 export const SKILLS = [
-  { name: 'ReactJs', initialRating: 3 },
+  { name: 'ReactJs', initialRating: 4 },
   // { name: 'React-Native', initialRating: 4 },
-  { name: 'JavaScript', initialRating: 3 },
+  { name: 'JavaScript', initialRating: 4 },
   // { name: 'Redux', initialRating: 4 },
   // { name: 'Shopify/Reactjs', initialRating: 4 },
-  { name: 'Nodejs', initialRating: 3 },
-  { name: 'Expressjs', initialRating: 3 },
-  { name: 'MongoDB', initialRating: 3 },
+  { name: 'Nodejs', initialRating: 4 },
+  { name: 'Expressjs', initialRating: 4 },
+  { name: 'MongoDB', initialRating: 4 },
   // { name: 'Koajs', initialRating: 3 },
   { name: 'Html', initialRating: 4 },
   { name: 'CSS', initialRating: 4 },
@@ -102,7 +48,7 @@ export const SKILLS = [
   // { name: 'Apollo', initialRating: 4 },
   // { name: 'AntD | ReactStrap | Material UI', initialRating: 4 },
   // { name: 'Shopify Polaris', initialRating: 4 },
-  { name: 'Git', initialRating: 3 },
+  { name: 'Git', initialRating: 4 },
 ];
 
 export const TOOLS = [
